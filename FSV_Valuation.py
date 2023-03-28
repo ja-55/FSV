377f70106b5f13fe0c6d7784297c1fd18a7beb2e
Jaison  Abraham <87917798+ja-55@users.noreply.github.com>
2023-03-27 19:22:23 -0500

    Initialization

---
+++ FSV_Valuation.py
@@ -0,0 +1,38 @@
+### IMPORTS
+
+import pandas as pd
+
+### INPUTS
+
+years = range(2023,2028)
+
+### DATAFRAME SETUPS
+
+ebita = pd.DataFrame(columns = years,
+                      index = ['1. Revenue','2. -COS','3. -SGA',
+                               '4. +Op Lease Int','5. xEBITA'])
+
+noplat = pd.DataFrame(columns = years,
+                      index = ['1. EBITA', '2. -Cash Taxes', '3. xNOPLAT'])
+
+gross_cf = pd.DataFrame(columns = years,
+                      index = ['1. NOPLAT','2. -Depreciation','3. xGross CF',])
+
+gross_inv = pd.DataFrame(columns = years,
+                         index = ['1. +Delta Op WC','2. +Net Capex',
+                                  '3. +Delta Op Lease Debt','4. +Intangible Investment',
+                                  '5. +Misc Investment', '6. xGross Investment'])
+
+### CALCULATIONS
+
+ebita['5. xEBITA'] = ebita.sum()
+
+noplat['1. EBITA'] = ebita['5. xEBITA']
+noplat['3. xNOPLAT'] = noplat.sum()
+
+gross_cf['1. NOPLAT'] = noplat['3. xNOPLAT']
+gross_cf['3. xGross CF'] = gross_cf.sum()
+
+gross_inv['6. xGross Investment'] = gross_inv.sum()
+
+fcf = gross_cf['3. xGross CF'] - gross_inv['6. xGross Investment']
