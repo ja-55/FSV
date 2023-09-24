fb7c615e6863014a03671c7d8ea01b29efec50a1
Jaison  Abraham <87917798+ja-55@users.noreply.github.com>
2023-09-24 09:50:50 -0500

    Update FSV_Revenue.py
    Explore chi squared distribution for revenue growth

--- FSV_Revenue.py
+++ FSV_Revenue.py
@@ -1,12 +1,26 @@
 import pandas as pd
+import numpy as np
+from scipy.stats import chi2
 
+# READ IN DATA
 data = pd.read_excel('FSV_Input.xlsx', sheet_name = 'Grid')
-
 data.set_index('Desc', inplace = True)
 
+# VARIABLE SETUP
+hist_yr1 = data.columns[1]
 fcst_yr1 = data.columns[-1] + 1
 fcst_yrs = list(range(fcst_yr1, fcst_yr1 + 3))
 
+# ADD FORECAST COLUMNS
 data = data.reindex(columns = data.columns.tolist() + fcst_yrs)
 
-print(data)
+# GENERATE REVENUE GROWTH DISTRIBUTION
+
+#x = np.linspace(chi2.ppf(0.01, 51),
+#                chi2.ppf(0.99, 51), 100)
+
+
+# ADD REVENUE GROWTH ROW
+data.loc['Growth',:] = data.loc['Revenue',:].pct_change()
+
+#print(x)
