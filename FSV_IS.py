bafa5477d8d51c9d765cb4a59d872bcec4015c6a
Jaison  Abraham <87917798+ja-55@users.noreply.github.com>
2023-11-22 12:20:04 -0600

    FSV Update
    Added html and JS files. Added to income statement model.

--- FSV_IS.py
+++ FSV_IS.py
@@ -6,10 +6,17 @@ from scipy.stats import norm
 import matplotlib.pyplot as plt
 
 
+# NOTES
+
+# python -m http.server
+
+
+
 # READ IN DATA
 data = pd.read_excel('FSV_Input.xlsx', sheet_name = 'Grid')
+main_is = pd.DataFrame(columns = data.columns)
 
-main_is = data.copy()
+data.set_index('Desc', inplace = True)
 main_is.set_index('Desc', inplace = True)
 
 # VARIABLE SETUP
@@ -31,8 +38,16 @@ dist_gr_rev = skewnorm(a, loc, scale).rvs(1000)
 # SET UP METRICS 
 metr_is = pd.DataFrame(columns = main_is.columns.tolist())
 
+# ADD ACTUALS TO INCOME STATEMENT DATAFRAME
+for yr in main_is.columns:
+    
+    if yr < fcst_yr1:
+        main_is.loc['Revenue', yr] = data.loc['Revenue', yr]
+    else:
+        pass
+
 # ADD REVENUE GROWTH LINE
-metr_is.loc['Growth_Revenue',:] = main_is.loc['Revenue',:].pct_change()
+metr_is.loc['Growth_Revenue',:] = data.loc['Revenue',:].pct_change()
 
 for yr in fcst_yrs:
     metr_is.loc['Growth_Revenue', yr] = np.random.choice(dist_gr_rev, 1)
@@ -41,6 +56,14 @@ for yr in fcst_yrs:
 
 # GROSS MARGIN
 
+
+# ADD ACTUALS TO INCOME STATEMENT DATAFRAME
+for yr in main_is.columns:
+    
+    if yr < fcst_yr1:
+        main_is.loc['COS', yr] = data.loc['COS', yr]
+    else:
+        pass
 
 # GENERATE GROSS MARGIN DISTRIBUTION (Normal distribution)
 dist_mn_gross = norm(loc = 0.326, scale = 0.01).rvs(1000)
@@ -56,6 +79,14 @@ for yr in fcst_yrs:
 # OPERATING EXPENSES (NON-DEPRECIATION)
 
 
+# ADD ACTUALS TO INCOME STATEMENT DATAFRAME
+for yr in main_is.columns:
+    
+    if yr < fcst_yr1:
+        main_is.loc['Opex_NonDepr', yr] = data.loc['Opex_NonDepr', yr]
+    else:
+        pass
+
 # GENERATE DISTRIBUTION (Normal distribution)
 dist_mn_opex_nondepr = norm(loc = 0.125, scale = 0.01).rvs(1000)
 
@@ -67,18 +98,26 @@ for yr in fcst_yrs:
     main_is.loc['Opex_NonDepr', yr] = main_is.loc['Revenue', yr] * metr_is.loc['Margin_Opex_NonDepr', yr]
 
 
-# DEPRECIATION / AMORTIZATION
+# DEPRECIATION EXPENSES
 
+
+# ADD ACTUALS TO INCOME STATEMENT DATAFRAME
+for yr in main_is.columns:
+    
+    if yr < fcst_yr1:
+        main_is.loc['Depr', yr] = data.loc['Depr', yr]
+    else:
+        pass
 
 # GENERATE DISTRIBUTION (Normal distribution)
-dist_mn_depr = norm(loc = 0.032, scale = 0.0025).rvs(1000)
+dist_mn_opex_depr = norm(loc = 0.033, scale = 0.005).rvs(1000)
 
 # ADD OPEX NON-DEPR MARGIN LINE
-metr_is.loc['Margin_Depr',:] = main_is.loc['Depr',:] / main_is.loc['Revenue',:]
+metr_is.loc['Margin_Opex_Depr',:] = main_is.loc['Depr',:] / main_is.loc['Revenue',:]
 
 for yr in fcst_yrs:
-    metr_is.loc['Margin_Depr', yr] = np.random.choice(dist_mn_depr, 1)
-    main_is.loc['Depr', yr] = main_is.loc['Revenue', yr] * metr_is.loc['Margin_Depr', yr]
+    metr_is.loc['Margin_Opex_Depr', yr] = np.random.choice(dist_mn_opex_depr, 1)
+    main_is.loc['Depr', yr] = main_is.loc['Revenue', yr] * metr_is.loc['Margin_Opex_Depr', yr]
 
 
 
@@ -92,6 +131,4 @@ for yr in fcst_yrs:
 
 
 
-
-
-print(metr_is)
+print(main_is)
