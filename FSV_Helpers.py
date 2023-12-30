4b43e09b3ca17c5320ea2126c84701312ac5e3a7
Jaison  Abraham <87917798+ja-55@users.noreply.github.com>
2023-12-30 11:02:28 -0600

    FSV update
    Split out helper functions and built depreciation / PPE forecasts.

--- FSV_Helpers.py
+++ FSV_Helpers.py
@@ -74,34 +74,31 @@ def fcst_mn_sga(fcst_is, fcst_metr, data, fcst_yrs, fcst_yr1,
 
 # DEPRECIATION
 
-def fcst_depr(fcst_is, , fcst_bs, fcst_metr, data, fcst_yrs, fcst_yr1,
+def fcst_depr(fcst_is, fcst_bs, fcst_metr, data, fcst_yrs, fcst_yr1,
              dst_loc = 0.12, dst_scale = 0.005):
 
-    # ADD ACTUALS TO INCOME STATEMENT DATAFRAME
+    # Add actuals to income statement / balance sheet dataframe
     for yr in fcst_is.columns:
         
         if yr < fcst_yr1:
             fcst_is.loc['Depr', yr] = data.loc['Depr', yr]
-            fcst_bs.loc['PPE_Gross', yr] = data.loc['PPE_Gross', yr]
-            fcst_bs.loc['Accum_Depr', yr] = data.loc['Accum_Depr', yr]
-            fcst_bs.loc['PPE_Net', yr] = fcst_bs.loc['PPE_Gross', yr] - fcst_bs.loc['Accum_Depr', yr]
         else: pass
 
-
-    # ADD HISTORICAL AVERAGE ASSET LIFE TO METRICS
+    # Add historical average asset life to metrics
     if yr < fcst_yr1:
-        fcst_metr.loc['AssetLife',:] = fcst_bs.loc['PPE_Gross', yr] / fcst_is.loc['Depr', yr]
+        fcst_metr.loc['AssetLife',:] = (fcst_bs.loc['PPE_Gross', yr] + fcst_bs.loc['IA_Gross', yr]) / fcst_is.loc['Depr', yr]
     
-    # CREATE DISTRIBUTION FOR ASSET LIFE
+    # Create distribution for average asset life
     dst_loc = fcst_metr.loc['AssetLife',:(fcst_yr1 - 1)].mean()
     dst_scale = fcst_metr.loc['AssetLife',:(fcst_yr1 - 1)].std()
     dst = norm(loc = dst_loc, scale = dst_scale).rvs(1000)
 
-    # ADD FORECAST AVERAGE ASSET LIFE TO METRICS
-    if yr >= fcst_yr1:
+    # Add forecast of average asset life to metrics
+    for yr in fcst_yrs:
         fcst_metr.loc['AssetLife',yr] = np.random.choice(dst, 1)
 
-    # NEED GROSS PPE FORECAST TO FINISH
+    # Calculate depreciation
+    fcst_is.loc['Depr', yr] = fcst_metr.loc['AssetLife',yr] * (fcst_bs.loc['PPE_Gross', (yr - 1)] + fcst_bs.loc['IA_Gross', (yr - 1)])
 
     return (fcst_is, fcst_bs, fcst_metr)
 
@@ -132,72 +129,18 @@ def fcst_costdebt(fcst_is, fcst_bs, fcst_metr, data, fcst_yrs, fcst_yr1,
 
     return (fcst_is, fcst_bs, fcst_metr)
 
-# ACCOUNTS RECEIVABLE / INVENTORY / ACCOUNTS PAYABLE
-
-def fcst_turnratios(fcst_is, fcst_bs, fcst_metr, data, fcst_yrs, fcst_yr1,
-                    turn_metric, turn_base):
-
-    turn_name = 'Turn_' + turn_metric
-
-    # ADD ACTUALS TO INCOME STATEMENT / BALANCE SHEET DATAFRAME
-    for yr in fcst_bs.columns:
-        
-        if yr < fcst_yr1:
-            fcst_bs.loc[turn_metric, yr] = data.loc[turn_metric, yr]
-            fcst_metr.loc[(turn_name),yr] = fcst_is.loc[turn_base, yr] / fcst_bs.loc[turn_metric, yr]
-        else: pass
-
-    # GENERATE TURNOVER DISTRIBUTION (Normal distribution)
-    dst_loc = fcst_metr.loc[turn_name,:(fcst_yr1 - 1)].mean()
-    dst_scale = fcst_metr.loc[turn_name,:(fcst_yr1 - 1)].std()
-    dst = norm(loc = dst_loc, scale = dst_scale).rvs(1000)
-
-    # FORECAST TURNOVER AND TURN METRIC
-    for yr in fcst_yrs:
-        fcst_metr.loc[turn_name, yr] = np.random.choice(dst, 1)
-        fcst_bs.loc[turn_metric, yr] = fcst_is.loc[turn_base, yr] / fcst_metr.loc[turn_name, yr]
-
-    return (fcst_is, fcst_bs, fcst_metr)
-
-# OTHER BALANCE SHEET PERCENT OF REV FORECASTS (Other CA, Other NCA, Other CL, Other NCL)
-
-def fcst_pctofrev(fcst_is, fcst_bs, fcst_metr, data, fcst_yrs, fcst_yr1, stmt_name):
-
-    metr_name = 'POR_' + stmt_name
-
-    # ADD ACTUALS TO INCOME STATEMENT / BALANCE SHEET DATAFRAME
-    for yr in fcst_bs.columns:
-
-        if yr < fcst_yr1:
-            fcst_bs.loc[stmt_name, yr] = data.loc[stmt_name, yr]
-            fcst_metr.loc[(metr_name),yr] = fcst_bs.loc[metr_name, yr] / fcst_is.loc['Revenue', yr]
-        else: pass
-
-    # GENERATE TURNOVER DISTRIBUTION (Normal distribution)
-    dst_loc = fcst_metr.loc[metr_name,:(fcst_yr1 - 1)].mean()
-    dst_scale = fcst_metr.loc[metr_name,:(fcst_yr1 - 1)].std()
-    dst = norm(loc = dst_loc, scale = dst_scale).rvs(1000)
-
-    # FORECAST TURNOVER AND TURN METRIC
-    for yr in fcst_yrs:
-        fcst_metr.loc[metr_name, yr] = np.random.choice(dst, 1)
-        fcst_bs.loc[stmt_name, yr] = fcst_is.loc['Revenue', yr] * fcst_metr.loc[metr_name, yr]
-
-    return (fcst_is, fcst_bs, fcst_metr)
 
 
 
 # OTHER SIMPLE FORECASTS
 
-def fcst_oth(fcst_is, fcst_bs, fcst_metr, data, fcst_yrs, fcst_yr1):
+def fcst_goodwill(fcst_bs, data, fcst_yr1):
 
     # GOODWILL & INTANGBILE ASSETS (No variability)
     for yr in fcst_bs.columns:
         if yr < fcst_yr1:
             fcst_bs.loc['Goodwill', yr] = data.loc['Goodwill', yr]
-            fcst_bs.loc['Intangibles', yr] = data.loc['Intangibles', yr]
         elif yr >= fcst_yr1:
             fcst_bs.loc['Goodwill', yr] = data.loc['Goodwill', (fcst_yr1 - 1)]
-            fcst_bs.loc['Intangibles', yr] = data.loc['Intangibles', (fcst_yr1 - 1)]
 
-    return (fcst_is, fcst_bs, fcst_metr)
+    return fcst_bs
---
+++ FSV_Helpers_BS.py
@@ -0,0 +1,113 @@
+# IMPORTS
+import pandas as pd
+import numpy as np
+from scipy.stats import norm
+
+# ACCOUNTS RECEIVABLE / INVENTORY / ACCOUNTS PAYABLE
+
+def fcst_turnratios(fcst_is, fcst_bs, fcst_metr, data, fcst_yrs, fcst_yr1,
+                    turn_metric, turn_base):
+
+    turn_name = 'Turn_' + turn_metric
+
+    # Add actuals to income statement / balance sheet dataframe
+    for yr in fcst_bs.columns:
+        
+        if yr < fcst_yr1:
+            fcst_bs.loc[turn_metric, yr] = data.loc[turn_metric, yr]
+            fcst_metr.loc[(turn_name),yr] = fcst_is.loc[turn_base, yr] / fcst_bs.loc[turn_metric, yr]
+        else: pass
+
+    # Generate turnover distribution (Normal distribution)
+    dst_loc = fcst_metr.loc[turn_name,:(fcst_yr1 - 1)].mean()
+    dst_scale = fcst_metr.loc[turn_name,:(fcst_yr1 - 1)].std()
+    dst = norm(loc = dst_loc, scale = dst_scale).rvs(1000)
+
+    # Forecast turnover and turn metric
+    for yr in fcst_yrs:
+        fcst_metr.loc[turn_name, yr] = np.random.choice(dst, 1)
+        fcst_bs.loc[turn_metric, yr] = fcst_is.loc[turn_base, yr] / fcst_metr.loc[turn_name, yr]
+
+    return (fcst_is, fcst_bs, fcst_metr)
+
+# FIXED ASSETS & INTANGIBLE ASSETS (GROSS AND NET)
+
+def fcst_fixedassets(fcst_bs, fcst_cf, fcst_metr, data, fcst_yrs, fcst_yr1, stmt_name):
+
+    # Add actuals to balance sheet dataframe
+    for yr in fcst_bs.columns:
+        
+        if yr < fcst_yr1:
+            fcst_bs.loc['PPE_Gross', yr] = data.loc['PPE_Gross', yr]
+            fcst_bs.loc['PPE_Net', yr] = data.loc['PPE_Net', yr]
+            fcst_bs.loc['IA_Gross', yr] = data.loc['IA_Gross', yr]
+            fcst_bs.loc['IA_Net', yr] = data.loc['IA_Net', yr]
+
+            # Calculate retirements for actual years except year 1
+            try:
+                fcst_metr.loc['FA_Rtmt', yr] = fcst_bs.loc['PPE_Gross', yr] - fcst_bs.loc['PPE_Gross', (yr - 1)] - fcst_cf.loc['Capex', yr]
+                fcst_metr.loc['FA_Rtmt_Pct', yr] = fcst_metr.loc['FA_Rtmt', yr] / fcst_bs.loc['PPE_Gross', (yr - 1)]
+                fcst_metr.loc['IA_Rtmt', yr] = fcst_bs.loc['IA_Gross', yr] - fcst_bs.loc['IA_Gross', (yr - 1)] - fcst_cf.loc['IA_Adds', yr]
+                fcst_metr.loc['IA_Rtmt_Pct', yr] = fcst_metr.loc['IA_Rtmt', yr] / fcst_bs.loc['IA_Gross', (yr - 1)]
+            except: pass
+
+        else: pass
+
+    # Create distribution for retirements as a percentage of gross PPE
+    dst_fa_loc = fcst_metr.loc['FA_Rtmt_Pct',:(fcst_yr1 - 1)].mean()
+    dst_fa_scale = fcst_metr.loc['FA_Rtmt_Pct',:(fcst_yr1 - 1)].std()
+    dst_fa = norm(loc = dst_fa_loc, scale = dst_fa_scale).rvs(1000)
+
+    # Create distribution for retirements as a percentage of gross PPE
+    dst_ia_loc = fcst_metr.loc['IA_Rtmt_Pct',:(fcst_yr1 - 1)].mean()
+    dst_ia_scale = fcst_metr.loc['IA_Rtmt_Pct',:(fcst_yr1 - 1)].std()
+    dst_ia = norm(loc = dst_ia_loc, scale = dst_ia_scale).rvs(1000)
+
+    # Calculate forecast of retirements and PPE (gross and net)
+    for yr in fcst_yrs:
+
+        # Fixed assets
+        fcst_metr.loc['FA_Rtmt_Pct', yr] = np.random.choice(dst_fa, 1)
+        fcst_metr.loc['FA_Rtmt', yr] = fcst_bs.loc['PPE_Gross', (yr - 1)] * fcst_metr.loc['FA_Rtmt_Pct', yr]
+        fcst_bs.loc['PPE_Gross', yr] = fcst_bs.loc['PPE_Gross', (yr - 1)] + fcst_cf.loc['Capex', yr] + fcst_metr.loc['FA_Rtmt', yr]
+
+        # Intangible assets
+        fcst_metr.loc['IA_Rtmt_Pct', yr] = np.random.choice(dst_fa, 1)
+        fcst_metr.loc['IA_Rtmt', yr] = fcst_bs.loc['IA_Gross', (yr - 1)] * fcst_metr.loc['IA_Rtmt_Pct', yr]
+        fcst_bs.loc['IA_Gross', yr] = fcst_bs.loc['IA_Gross', (yr - 1)] + fcst_cf.loc['IA_Adds', yr] + fcst_metr.loc['IA_Rtmt', yr]
+
+
+    return (fcst_bs, fcst_cf, fcst_metr)
+
+
+    
+
+    
+
+
+
+# OTHER BALANCE SHEET PERCENT OF REV FORECASTS (Other CA, Other NCA, Other CL, Other NCL)
+
+def fcst_pctofrev(fcst_is, fcst_bs, fcst_metr, data, fcst_yrs, fcst_yr1, stmt_name):
+
+    metr_name = 'POR_' + stmt_name
+
+    # Add actuals to income statement / balance sheet dataframe
+    for yr in fcst_bs.columns:
+
+        if yr < fcst_yr1:
+            fcst_bs.loc[stmt_name, yr] = data.loc[stmt_name, yr]
+            fcst_metr.loc[(metr_name),yr] = fcst_bs.loc[metr_name, yr] / fcst_is.loc['Revenue', yr]
+        else: pass
+
+    # Generate distribution (Normal distribution)
+    dst_loc = fcst_metr.loc[metr_name,:(fcst_yr1 - 1)].mean()
+    dst_scale = fcst_metr.loc[metr_name,:(fcst_yr1 - 1)].std()
+    dst = norm(loc = dst_loc, scale = dst_scale).rvs(1000)
+
+    # Forecast metric
+    for yr in fcst_yrs:
+        fcst_metr.loc[metr_name, yr] = np.random.choice(dst, 1)
+        fcst_bs.loc[stmt_name, yr] = fcst_is.loc['Revenue', yr] * fcst_metr.loc[metr_name, yr]
+
+    return (fcst_is, fcst_bs, fcst_metr)
